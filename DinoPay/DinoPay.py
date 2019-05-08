f606f4c09b4a0e904724f128d86d53ef859f31fe
efri-gomspace <efri@gomspace.com>
2019-05-08 14:48:05 +0200

    Update

--- DinoPay/DinoPay.py
+++ DinoPay/DinoPay.py
@@ -1,18 +1,33 @@
 import logging
+import requests
+from python_hmac_auth import HmacAuth
 import json
 
 
 
-
-__status__  = "production"
-# The following module attributes are no longer updated.
-
-__date__    = "28042019"
-__version__ = "1.1_" +__date__
-
-
+class mpif():
+    def __init__(self):
+        self.logger = logging
+        self.url = 'https://sandprod-pos2.mobilepay.dk/API/V08/RegisterPoS'
+        self.headers={'content-type':'application/json','Authorization': 'zSQQxEXEqS1ETbxyAFmq8vDRTkWfg3LQ29bsCx2Bqm4= 1557255784'}
+        self.MerchantId = "POSDK64549"
+        self.LocationId = "66001"
+        self.PosId = ""
+        self.Name = "Skattekisten"
+        auth=HmacAuth('36619E65-9C01-4B94-92CD-2B22A041CEFC', 'self.url')
+        
+        
+    def reqResp(self):
+    ##parsing response
+        
+        r = requests.get(url = self.url, data={'MerchantId': self.MerchantId, 'LocationId':self.LocationId, 'PosId': self.PosId, 'name': self.Name})
+        print(r.status_code, r.reason)
+        print(r)
+       
 if __name__ == '__main__':
     try:
+        mp = mpif()
+        mp.reqResp()
     except Exception as e:
         logging.error("main exception:" +str(e))
         
