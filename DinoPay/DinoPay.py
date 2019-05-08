1ce3525a08e2744e78c69d31735aa80941054dc5
Esben Frihauge <esj@frihauge.dk>
2019-05-08 10:55:56 +0200

    Update

---
+++ DinoPay/DinoPay.py
@@ -0,0 +1,19 @@
+import logging
+import json
+
+
+
+
+__status__  = "production"
+# The following module attributes are no longer updated.
+
+__date__    = "28042019"
+__version__ = "1.1_" +__date__
+
+
+if __name__ == '__main__':
+    try:
+    except Exception as e:
+        logging.error("main exception:" +str(e))
+        
+    
